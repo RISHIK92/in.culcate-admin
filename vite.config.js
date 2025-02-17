54d36444b6c205b0db97c3c3faa1b4539fc3639c
Rishik <rishik3555@gmail.com>
2025-02-17 10:11:23 +0530

    repo

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // Example base URL
+  base: "./",
   plugins: [react()],
-  server: {
-    historyApiFallback: true, // Enable history fallback
-  },
-})
+});
