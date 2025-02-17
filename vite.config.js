90d6c7edfd88ee41590250e8591a2daa08bfd2e3
Rishik <rishik3555@gmail.com>
2025-02-17 15:20:54 +0530

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/", // Ensure this is set correctly
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Ensures React Router can handle routes
+  },
 });
