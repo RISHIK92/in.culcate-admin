9c9cf4b6686975860b9367144e4d1ee87d51b321
Rishik <rishik3555@gmail.com>
2025-02-17 15:36:15 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import './index.css'
 import { Signup } from './pages/signup'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import { Signin } from './pages/signin'
 import { Dashboard } from './pages/dashboard'
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/signin" replace />} />
         <Route path='/signup' element={<Signup />}/>
         <Route path='/signin' element={<Signin />}/>
         <Route path='/dashboard' element={<Dashboard />}/>
+        <Route path="*" element={<Navigate to="/signin" replace />} />
       </Routes>
     </BrowserRouter>
   )
