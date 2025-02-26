1dfeb53a68ab6626772a03915dcb20983a9ef22b
Rishik <rishik3555@gmail.com>
2025-02-26 16:47:21 +0530

    fixed

--- src/Url.js
+++ src/Url.js
@@ -1,3 +1,3 @@
 // export const BACKEND_URL = "https://admin.error6o6.tech/api/admin/v1/"
 // export const BACKEND_URL = "https://admin-backend-fc1z.onrender.com/api/admin/v1/"
-export const BACKEND_URL = "https://rail.admin.error6o6.tech/api/admin/v1"
+export const BACKEND_URL = "https://rail.admin.error6o6.tech/api/admin/v1/"
